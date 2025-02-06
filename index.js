4a51c23c47b96e6b396c8c124af32432a5810d43
jannebou <Janne.sainio15@gmail.com>
2025-02-06 11:09:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -164,6 +164,7 @@ const generateId = () => {
 
 const PORT = 3001
 app.listen(PORT, () => {
+  sql`DELETE FROM list`.catch(error => {})
   sql`CREATE TABLE IF NOT EXISTS list (id SERIAL PRIMARY KEY, item TEXT)`.catch(error => {})
   console.log(`Server running on port ${PORT}`)
 })
